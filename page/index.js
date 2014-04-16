09482b6a99976daf6492753bae07fdfda36f68ba
rogeriopvl <rogeriopvl@gmail.com>
2014-04-16 12:13:40 +0100

    Catch ENOENT on layout subgenerator and print friendly message

--- page/index.js
+++ page/index.js
@@ -8,7 +8,6 @@ var PageGenerator = module.exports = function PageGenerator(args, options, confi
   yeoman.generators.NamedBase.apply(this, arguments);
 
   this.fileName = args[1] || null;
-
 };
 
 util.inherits(PageGenerator, yeoman.generators.NamedBase);
@@ -17,5 +16,13 @@ PageGenerator.prototype.files = function files() {
   var prefix = Math.floor(new Date().getTime() / 1000),
       newName = this.fileName || prefix + '_' + this.name + '.html';
 
-  this.copy('../../app/templates/_layouts/' + this.name + '.html', 'app/' + newName);
+  try {
+    this.copy('../../app/templates/_layouts/' + this.name + '.html', 'app/' + newName);
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      console.log('%s: layout not found', this.name);
+    } else {
+      throw e;
+    }
+  }
 };
